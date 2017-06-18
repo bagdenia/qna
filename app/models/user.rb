fc5d115d3e81911f515c3cffe1ee9fdd99232f57
bagdenia <bagdenia@gmail.com>
2017-06-18 18:40:50 +0300

    after merge

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  def voted_this(item)
-    votes.where('votable_type = ? and votable_id = ?', item.class.name, item.id).size > 0
-  end
-
-  def find_vote(item)
-    votes.where('votable_type = ? and votable_id = ?', item.class.name, item.id).first
-  end
 end
