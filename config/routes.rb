31c2a7b4ea1336c672770e28655359bba0bb2f65
bagdenia <bagdenia@gmail.com>
2017-06-18 18:35:48 +0300

    after merge

--- config/routes.rb
+++ config/routes.rb
@@ -4,18 +4,12 @@ Rails.application.routes.draw do
   resources :votes, only: [:create, :destroy]
 
   resources :questions, shallow: true do
-    # resources :votes, only: [:create, :destroy], defaults:{ votable_type: 'Question'}
     resources :attachments
     resources :answers, shallow: true do
-<<<<<<< HEAD
-=======
-      # resources :votes, only: [:create, :destroy], defaults:{ votable_type: 'Answer'}
->>>>>>> Voting fixed
       resources :attachments
       patch :set_best, on: :member
     end
   end
 
   root to: "questions#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
