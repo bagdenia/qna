017bed7b623ac3dabe7957b415cf8e78bf546d57
bagdenia <bagdenia@gmail.com>
2017-06-19 13:08:21 +0300

    Defaultes added to votes routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   # resources :votes, only: [:create, :destroy]
 
   resources :questions, shallow: true do
-    resources :votes, only: [:create, :destroy]#, defaults: {votable: 'Questions'}
+    resources :votes, only: [:create, :destroy], defaults: {votable: 'questions'}
     resources :attachments
     resources :answers, shallow: true do
-      resources :votes, only: [:create, :destroy]#, defaults: {votable: 'Answers'}
+      resources :votes, only: [:create, :destroy], defaults: {votable: 'answers'}
       resources :attachments
       patch :set_best, on: :member
     end
