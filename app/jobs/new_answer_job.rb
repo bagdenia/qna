388258d512854ed071649335dcd395d5b9373538
bagdenia <bagdenia@gmail.com>
2017-07-25 15:45:38 +0300

    Active job fixes

--- app/jobs/new_answer_job.rb
+++ app/jobs/new_answer_job.rb
@@ -4,7 +4,7 @@ class NewAnswerJob < ApplicationJob
   def perform(answer)
     answer.question.subscriptions.map{|sub| sub.user}.each do |user|
       if answer.user != user
-        AnswerMailer.informer(answer, user).deliver_now
+        AnswerMailer.informer(answer, user).deliver_later #ну и они перестали отправляться с later, шеф все пропало
       end
     end
     # Do something later
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -4,4 +4,6 @@ class Subscription < ApplicationRecord
 
   validates :user, :question, presence: true
   validates :question_id, uniqueness: {scope: :user_id}
+
+  # scope :with_admin_user,  lambda { joins(:user).where(users: {admin: true}) }
 end
