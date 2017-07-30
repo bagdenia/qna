5fef34b4d7dd4cd45cfc6d4e2eb467976ca8ce55
bagdenia <bagdenia@gmail.com>
2017-07-30 10:43:47 +0300

    Simplify rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,8 +8,6 @@ require 'rspec/rails'
 require 'capybara/email/rspec'
 require 'cancan/matchers'
 require 'sidekiq/testing'
-require 'thinking_sphinx/test'
-ThinkingSphinx::Test.init
 
 Sidekiq::Testing.fake!
 #require 'shoulda/matchers'
@@ -48,25 +46,6 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
-  config.before :each do |example|
-    # Configure and start Sphinx for request specs
-    if example.metadata[:type] == :request
-      ThinkingSphinx::Test.init
-      ThinkingSphinx::Test.start index: false
-    end
-
-    # Disable real-time callbacks if Sphinx isn't running
-    ThinkingSphinx::Configuration.instance.settings['real_time_callbacks'] =
-      (example.metadata[:type] == :request)
-  end
-
-  config.after(:each) do |example|
-    # Stop Sphinx and clear out data after request specs
-    if example.metadata[:type] == :request
-      ThinkingSphinx::Test.stop
-      ThinkingSphinx::Test.clear
-    end
-  end
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
